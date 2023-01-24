1c51f558e79570c85602f61a8428cc9c410a6bb3
mahes1287 <mahes1287@gmail.com>
2023-01-24 23:47:13 +0100

    initial model for crud

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Translations(models.Model):
+    id = models.AutoField(primary_key=True)
+    input = models.CharField(max_length=500)
+    output = models.CharField(max_length=500)
+    fromUser= models.CharField(max_length=500)
+    # fromUser= models.ForeignKey(User, verbose_name="user")
+
+
+# TODO create User and connect it, then update the fromUser field
