2ddfc4f583980ad0bb8506c8b319f0195ee4aa90
mahes1287 <mahes1287@gmail.com>
2023-01-25 11:17:33 +0100

    create translation implementation

--- crud/urls.py
+++ crud/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('translations', views.getTranslations, name="getAllTranslations")
+    path('translations', views.getTranslations, name="getAllTranslations"),
+    path('translations/create', views.createTranslation, name="createTranslation")
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
 from .serializers import TranslationSerializer
 from .models import Translation
+from rest_framework.parsers import JSONParser
 # Create your views here.
 
 def index(request):
@@ -14,3 +15,14 @@ def getTranslations(request):
     translation = Translation.objects.all()
     serializer = TranslationSerializer(translation, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+@parser_classes([JSONParser])
+def createTranslation(request):
+    data = request.data
+    print(data)
+    translation = Translation.objects.create(**data)
+    serializer = TranslationSerializer(translation, many=False)
+    return Response(serializer.data)
+
+# TODO ensure the response data cntain proper status code.
