bc7c76d0e53e22c4618264e9e22fec4a71a00680
mahes1287 <mahes1287@gmail.com>
2023-01-25 10:51:37 +0100

    renamed crud url to api

--- base/urls.py
+++ base/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('crud/', include('crud.urls')),
+    path('api/', include('crud.urls')),
     path('admin/', admin.site.urls),
 ]
