4fb3bc56b65469c83c0f9dcc3badfda95091a733
mahes1287 <mahes1287@gmail.com>
2023-01-25 11:40:36 +0100

    added formatter for vscode

--- crud/urls.py
+++ crud/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from . import views 
+from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('translations', views.getTranslations, name="getAllTranslations"),
-    path('translations/create', views.createTranslation, name="createTranslation")
+    path("", views.index, name="index"),
+    path("translations/", views.getTranslations, name="getAllTranslations"),
+    path("translations/create", views.createTranslation, name="createTranslation"),
+    path("translations/<str:pk>/", views.getOneTranslation, name="getOneTranslation"),
+    path(
+        "translations/<str:pk>/update",
+        views.updateTranslation,
+        name="updateTranslation",
+    ),
+    # path('translations/<str:pk>/delete', views.updateTranslation, name="deleteTranslation"),
 ]
+
+# TODO set things for 404 response for invalid urls
--- crud/views.py
+++ crud/views.py
@@ -5,18 +5,29 @@ from rest_framework.decorators import api_view, parser_classes
 from .serializers import TranslationSerializer
 from .models import Translation
 from rest_framework.parsers import JSONParser
+
 # Create your views here.
+
 
 def index(request):
     return HttpResponse("You reached top of Simple Crud App")
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def getTranslations(request):
     translation = Translation.objects.all()
     serializer = TranslationSerializer(translation, many=True)
     return Response(serializer.data)
 
-@api_view(['POST'])
+
+@api_view(["GET"])
+def getOneTranslation(request, pk):
+    translation = Translation.objects.get(id=pk)
+    serializer = TranslationSerializer(translation, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
 @parser_classes([JSONParser])
 def createTranslation(request):
     data = request.data
@@ -24,6 +35,28 @@ def createTranslation(request):
     translation = Translation.objects.create(**data)
     serializer = TranslationSerializer(translation, many=False)
     return Response(serializer.data)
+
+
+@api_view(["PUT"])
+@parser_classes([JSONParser])
+def updateTranslation(request, pk):
+    data = request.data
+    translation = Translation.objects.get(id=pk)
+    print(data)
+    serializer = TranslationSerializer(instance=translation, data=data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+# @api_view(['DELETE'])
+# @parser_classes([JSONParser])
+# def updateTranslation(request):
+#     data = request.data
+#     print(data)
+#     translation = Translation.objects.create(**data)
+#     serializer = TranslationSerializer(translation, many=False)
+#     return Response(serializer.data)
 
 # TODO ensure the response data cntain proper status code.
 # TODO error handling is pending
