a28c192e663e7169bb270292c70ffba6fcdcc99d
mahes1287 <mahes1287@gmail.com>
2023-01-29 20:43:06 +0100

    first test...

--- crud/tests.py
+++ crud/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexTest(APITestCase):
+    def test_index_api(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
