b0448b34dc08b4fde251d62b7dbb23d2ba6685f1
mahes1287 <mahes1287@gmail.com>
2023-01-29 13:14:50 +0100

    status code corrected

--- firebase_auth/exceptions.py
+++ firebase_auth/exceptions.py
@@ -1,19 +1,20 @@
 from rest_framework.exceptions import APIException
+from rest_framework import status
 
 
 class FirebaseAuthException(APIException):
-    status = 500
+    status_code = 500
     default_detail = "Firebase auth exception"
     default_code = "firebase_auth"
 
 
 class TokenNotFound(APIException):
-    status = 401
+    status_code = status.HTTP_404_NOT_FOUND
     default_detail = "Credentials not found, need to login"
     default_code = "token_not_found"
 
 
 class InvalidToken(APIException):
-    status = 401
+    status_code = 401
     default_detail = "Invalid Token"
     default_code = "invalid_token"
