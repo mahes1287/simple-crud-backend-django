49e7c83cb597b442e255e55243fe49be3fa5b27e
mahes1287 <mahes1287@gmail.com>
2023-01-29 22:36:48 +0100

    scaffolding test cases

--- crud/tests.py
+++ crud/tests.py
@@ -3,7 +3,28 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 
 
-class IndexTest(APITestCase):
+class IndexTestCase(APITestCase):
     def test_index_api(self):
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["message"], "Hello api")
+
+
+class GetTranslationsTestCase(APITestCase):
+    def test_translations_list(self):
+        pass
+
+
+class CreateTranslationTestCase(APITestCase):
+    def test_create_translation(self):
+        pass
+
+
+class UpdateTranslationTestCase(APITestCase):
+    def test_update_translation(self):
+        pass
+
+
+class DeleteTranslationTestCase(APITestCase):
+    def test_update_translation(self):
+        pass
