69c435eeead87da47e83a85f6fce6f5707e4ba4a
mahes1287 <mahes1287@gmail.com>
2023-01-30 22:16:04 +0100

    changed response format

--- user/views.py
+++ user/views.py
@@ -1,8 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, parser_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from firebase_auth.permissions import AllowAll
+from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
 from .models import User
 from rest_framework.parsers import JSONParser
@@ -10,18 +9,44 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 @api_view(["GET"])
-def index(request):
+def index():
     response = {"message": "Hello from user app", "data": False, "error": False}
     return Response(data=response, status=status.HTTP_200_OK)
 
 
+@api_view(["POST"])
+@parser_classes([JSONParser])
+def CreateUser(request):
+    data = request.data
+    user = User.objects.create(**data)
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
 @api_view(["GET"])
+@permission_classes((IsAuthenticated,))
 def GetUserInfo(request, uid):
     try:
         user = User.objects.get(uid=uid)
         serializer = UserSerializer(user, many=False)
-        newdict = {"error": False, "message": "success"}
-        newdict.update(serializer.data)
+        newdict = {"error": False, "message": "success", "data": serializer.data}
+        return Response(newdict, status=status.HTTP_200_OK)
+
+    except ObjectDoesNotExist:
+        print("error")
+        return Response(
+            {"message": "Does not exist", "error": True},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+
+@api_view(["GET"])
+@permission_classes((IsAuthenticated,))
+def GetAllUserInfo(request):
+    try:
+        user = User.objects.all()
+        serializer = UserSerializer(user, many=True)
+        newdict = {"error": False, "message": "success", "data": serializer.data}
         return Response(newdict, status=status.HTTP_200_OK)
 
     except ObjectDoesNotExist:
@@ -29,14 +54,3 @@ def GetUserInfo(request, uid):
             {"message": "Does not exist", "error": True},
             status=status.HTTP_404_NOT_FOUND,
         )
-
-
-@api_view(["POST"])
-@parser_classes([JSONParser])
-def CreateUser(request):
-    data = request.data
-    print(data)
-    user = User.objects.create(**data)
-    print(user)
-    serializer = UserSerializer(user, many=False)
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
