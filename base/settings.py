1762c3c13adb43b804342a6d5551de8af7a2711b
mahes1287 <mahes1287@gmail.com>
2023-01-31 11:06:46 +0100

    os.getenv is replaced by os.environ

--- base/settings.py
+++ base/settings.py
@@ -7,9 +7,9 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = str(os.getenv.get("SECRET_KEY"))
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = False
+DEBUG = os.environ.get("DEBUG")
 
 print("debug", DEBUG)
 
@@ -118,6 +118,11 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    "/var/www/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
